c7f88b614077448c094d1998753dc20ac31aa327
Fabrizio W <fabrizio2210@gamil.com>
2025-03-15 18:31:05 +0000

    Handling error in bash

--- CICD.sh
+++ CICD.sh
@@ -1,10 +1,6 @@
 #!/bin/bash -x
 
-if [ -z "$ACTION" ]; then
-    echo "ACTION is not set"
-    ACTION=""
-fi
 
-if [ $ACTION = "insert_email" ]; then
+if [ "$ACTION" = "insert_email" ]; then
     python3 src/python/email_extractor/insert_email.py --db_uri ${DB_URI} --db_name cover_letter --email ${EMAIL_TO_INSERT}
 fi
