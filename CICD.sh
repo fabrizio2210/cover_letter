f45e6f010605bb7aa8b67b5bfd99bceb683c00fc
Fabrizio W <fabrizio2210@gamil.com>
2025-03-15 18:29:15 +0000

    Add default value for input

--- CICD.sh
+++ CICD.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -x
 
+if [ -z "$ACTION" ]; then
+    echo "ACTION is not set"
+    ACTION=""
+fi
 
 if [ $ACTION = "insert_email" ]; then
     python3 src/python/email_extractor/insert_email.py --db_uri ${DB_URI} --db_name cover_letter --email ${EMAIL_TO_INSERT}
