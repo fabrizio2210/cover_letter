7809427874e426ebda52606e0466d27801d96b85
Fabrizio W <fabrizio2210@gamil.com>
2025-03-30 18:36:37 +0100

    Add container

--- CICD.sh
+++ CICD.sh
@@ -1,5 +1,53 @@
 #!/bin/bash -x
 
+#############
+# Environment
+
+if [ $(uname -m) = "x86_64" ] ; then
+  arch="x86_64"
+  dockerArch="x86_64"
+else
+  arch="armv7hf"
+  dockerArch="armhf"
+fi
+
+changedFiles="$(git diff --name-only HEAD^1 HEAD)"
+
+################
+# Login creation
+
+if [ ! -f ~/.docker/config.json ] ; then 
+  mkdir -p ~/.docker/
+
+  if [ -z "$DOCKER_LOGIN" ] ; then
+	  echo "Docker login not found in the environment, set DOCKER_LOGIN"
+  else
+    cat << EOF > ~/.docker/config.json
+{
+  "experimental": "enabled",
+        "auths": {
+                "https://index.docker.io/v1/": {
+                        "auth": "$DOCKER_LOGIN"
+                }
+        },
+        "HttpHeaders": {
+                "User-Agent": "Docker-Client/17.12.1-ce (linux)"
+        }
+}
+EOF
+  fi
+fi
+
+#############
+# BUILD IMAGE
+
+if grep -q "Dockerfile" <<< "$changedFiles"; then
+  docker buildx build --platform linux/$dockerArch -t fabrizio2210/docker_light-cover_letter:$dockerArch --build-arg ARCH=$dockerArch --push -f docker/x86_64/Dockerfile-container .
+fi
+
+
+#############
+# Actions
 
 if [ "$ACTION" = "insert_email" ]; then
     python3 src/python/email_extractor/insert_email.py --db_uri ${DB_URI} --db_name cover_letter --email ${EMAIL_TO_INSERT}
--- src/python/email_extractor/insert_email.py
+++ src/python/email_extractor/insert_email.py
@@ -7,9 +7,17 @@ def insert_email(email, db_uri, db_name):
     db = client[db_name]
     collection = db["recipients"]
     
-    # Insert the email into the "recipients" collection
-    collection.insert_one({"email": email})
-    print(f"Inserted email: {email}")
+    # Insert the email into the "recipients" collection if it doesn't already exist
+    result = collection.update_one(
+        {"email": email},  # Filter to check if the email already exists
+        {"$setOnInsert": {"email": email}},  # Insert only if it doesn't exist
+        upsert=True  # Perform an upsert operation
+    )
+    
+    if result.upserted_id:
+        print(f"Inserted email: {email}")
+    else:
+        print(f"Email already exists: {email}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Insert an email into MongoDB")
