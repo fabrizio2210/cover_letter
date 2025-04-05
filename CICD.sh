57b015aafa570f21b202611ce8fbfd4173c6fe2f
Fabrizio W <fabrizio2210@gamil.com>
2025-04-05 16:38:42 +0100

    Add telegram_bot to regex

--- CICD.sh
+++ CICD.sh
@@ -44,6 +44,6 @@ fi
 if [ "$MANUAL" == "1" ] || grep -q "Dockerfile-container" <<< "$changedFiles"; then
   docker buildx build -t fabrizio2210/docker_light-cover_letter:$arch --push -f docker/x86_64/Dockerfile-container .
 fi
-if [ "$MANUAL" == "1" ] || grep -q "Dockerfile-bot" <<< "$changedFiles"; then
+if [ "$MANUAL" == "1" ] || grep -q "Dockerfile-bot\|telegram_bot" <<< "$changedFiles"; then
   docker buildx build -t fabrizio2210/coverletter-telegram-bot:$arch --push -f docker/x86_64/Dockerfile-bot .
 fi
