edce2ccdad7f7158d8b6770879267ab6260fa930
Fabrizio W <fabrizio2210@gamil.com>
2025-08-17 20:26:00 +0100

    Clean up

--- src/js/coverletter-frontend/src/app/app.component.ts
+++ src/js/coverletter-frontend/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from './login.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   template: '<app-login></app-login>',
   standalone: true,
-  imports: [LoginComponent, HttpClientModule]
+  imports: [LoginComponent]
 })
 export class AppComponent {}
--- src/js/coverletter-frontend/src/main.ts
+++ src/js/coverletter-frontend/src/main.ts
@@ -1,18 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-
-const bootstrapApp: typeof bootstrapApplication = bootstrapApplication;
-
-bootstrapApp(AppComponent)
-.catch((err: unknown): void => console.error(err));
-
-interface ErrorHandler {
-  (err: unknown): void;
-}
-
-const typedBootstrapApplication: typeof bootstrapApplication = bootstrapApplication;
-const typedErrorHandler: ErrorHandler = (err) => console.error(err);
-
-typedBootstrapApplication(AppComponent)
-  .catch(typedErrorHandler);
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err: unknown) => console.error(err));
